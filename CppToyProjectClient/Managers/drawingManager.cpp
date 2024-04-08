2e826600d0165c72b6c54d724a46644101c7a2a5
anaddict100@gmail.com <anaddict100@gmail.com>
2024-04-08 11:36:42 +0900

    declaration functions in drawingManager

--- CppToyProjectClient/Managers/drawingManager.cpp
+++ CppToyProjectClient/Managers/drawingManager.cpp
@@ -1,1 +1,25 @@
 #include "drawingManager.h"
+
+DrawingManager::DrawingManager(unsigned int windowWidth, unsigned int windowHeight) : m_windowWidth(windowWidth), m_windowHeight(windowHeight)
+{
+}
+
+void DrawingManager::drawMyBackground(HDC hdc)
+{
+}
+
+void DrawingManager::drawOtherBackground(HDC hdc)
+{
+}
+
+void DrawingManager::loadResources(HINSTANCE instance)
+{
+}
+
+void DrawingManager::drawBackground(HDC hdc)
+{
+	HDC memDC;
+
+	drawMyBackground(hdc);
+	drawOtherBackground(hdc);
+}
--- CppToyProjectClient/Managers/drawingManager.h
+++ CppToyProjectClient/Managers/drawingManager.h
@@ -1,5 +1,38 @@
 #pragma once
+#include "../framework.h"
+
 class DrawingManager
 {
+private:
+	static DrawingManager* m_instance;
+
+	DrawingManager* getInstance()
+	{
+		if (!m_instance)
+		{
+			m_instance = this;
+		}
+
+		return m_instance;
+	}
+
+private:
+	std::vector<HBITMAP> bitmaps;
+
+public:
+	DrawingManager() = delete;
+	DrawingManager(unsigned int windowWidth, unsigned int windowHeight);
+	
+private:
+	unsigned int m_windowWidth;
+	unsigned int m_windowHeight;
+
+private:
+	void drawMyBackground(HDC hdc);
+	void drawOtherBackground(HDC hdc);
+
+public:
+	void loadResources(HINSTANCE instance);
+	void drawBackground(HDC hdc);
 };
 
